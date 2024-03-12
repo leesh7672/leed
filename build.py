756b8d60829b7a3a74fcdd9c819b7272038714f4
leesh7672 <leesh7672@gmail.com>
2024-03-13 07:45:18 +0900

    Scripts needless now removed.

--- build.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from hms import hcl
-
-if __name__ == '__main__':
-    hclf = open('leed-entries.tex', 'w', encoding='utf-8')
-    hclf.write(hcl.build())
-    hclf.close()
